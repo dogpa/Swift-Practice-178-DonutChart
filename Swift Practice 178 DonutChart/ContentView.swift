d7da3048b941d470e55862a9f49ffb2902e92b01
Dogpa <dogpa0130@gmail.com>
2022-10-23 18:37:03 +0800

    Initial Commit

---
+++ Swift Practice 178 DonutChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 178 DonutChart
+//
+//  Created by Dogpa's MBAir M1 on 2022/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 178 DonutChart/Swift_Practice_178_DonutChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_178_DonutChartApp.swift
+//  Swift Practice 178 DonutChart
+//
+//  Created by Dogpa's MBAir M1 on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_178_DonutChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
